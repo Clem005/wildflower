523ebdeb579cbbf454d95afbf9fa2f1ec7af1dcf
Clem005 <clemchuksthabo@gmail.com>
2026-07-10 22:33:55 +0200

    feat: add orders@mywildflower.co.za to admin notification bcc list

--- app/actions/sendPurchase.ts
+++ app/actions/sendPurchase.ts
@@ -48,7 +48,7 @@ export async function sendPurchase(formData: FormData) {
     const data = await resend.emails.send({
       from: "Wildflower Orders <orders@mywildflower.co.za>",
       to: [email],
-      bcc: ["somaya@mywildflower.co.za"],
+      bcc: ["somaya@mywildflower.co.za", "orders@mywildflower.co.za"],
       subject: `Your Wildflower Order: ${product}`,
       html: customerHtmlContent,
     });
